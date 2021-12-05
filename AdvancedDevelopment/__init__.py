161700c5a44d65752699683367cdac17f93abc67
Ferby <ferby@gravitynetwork.net>
2021-12-05 17:43:21 +0000

    Remove MongoDB operations, set up for Firebase auth

--- AdvancedDevelopment/__init__.py
+++ AdvancedDevelopment/__init__.py
@@ -2,14 +2,14 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 
-cluster = MongoClient("mongodb+srv://s5124723:UJZ6xxtNrnYwSNWowyz8@advanceddevelopment.aiiro.mongodb.net/test")
-db = cluster["AdvancedDevelopment"]
-collection = db["test"]
+# cluster = MongoClient("mongodb+srv://s5124723:UJZ6xxtNrnYwSNWowyz8@advanceddevelopment.aiiro.mongodb.net/test")
+# db = cluster["AdvancedDevelopment"]
+# collection = db["test"]
 
-try:
-    post1 = {"_id": 1, "name": "Lucas", "score": 5}
-    post2 = {"_id": 2, "name": "Daniel", "score": 5}
-    collection.insert_many([post1, post2])
-except BulkWriteError as bwe:
-    print(bwe)
-
+# try:
+#     post1 = {"_id": 1, "name": "Lucas", "score": 5}
+#     post2 = {"_id": 2, "name": "Daniel", "score": 5}
+#     collection.insert_many([post1, post2])
+# except BulkWriteError as bwe:
+#     print(bwe)
+#
